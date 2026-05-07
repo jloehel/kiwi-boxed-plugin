a445a1d61b551ea08e0cb84387f496ba26b9cf6a
Marcus Schäfer <marcus.schaefer@gmail.com>
2026-05-07 12:05:52 +0200

    Bump version: 0.2.57 → 0.2.58

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -54,7 +54,7 @@ author = 'Marcus Schäfer'
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.57'
+version = '0.2.58'
 # The full version, including alpha/beta/rc tags.
 release = version
 
